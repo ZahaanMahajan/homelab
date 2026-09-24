ce11f7cac9b6b7988c91085fd0ddb86f68a36a58
Zahaan <zahaanjavaid032@gmail.com>
2026-09-24 15:53:05 +0530

    feat(proxmox): manage worker-02 VM with OpenTofu

--- infrastructure/proxmox/vms.tf
+++ infrastructure/proxmox/vms.tf
@@ -107,3 +107,41 @@ resource "proxmox_virtual_environment_vm" "worker_01" {
     firewall    = true
   }
 }
+
+resource "proxmox_virtual_environment_vm" "worker_02" {
+  name      = "worker-02"
+  node_name = "pve-i3"
+  vm_id     = 103
+
+  on_boot = false
+
+  cpu {
+    cores = 1
+    type  = "x86-64-v2-AES"
+  }
+
+  memory {
+    dedicated = 4096
+  }
+
+  bios = "seabios"
+
+  operating_system {
+    type = "l26"
+  }
+
+  scsi_hardware = "virtio-scsi-single"
+
+  disk {
+    datastore_id = "local-lvm"
+    interface    = "scsi0"
+    size         = 32
+    iothread     = true
+  }
+
+  network_device {
+    bridge      = "vmbr0"
+    mac_address = "BC:24:11:21:19:E4"
+    firewall    = true
+  }
+}
