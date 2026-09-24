da855428ac88124643d3623add4511ae1a61a4db
Zahaan <zahaanjavaid032@gmail.com>
2026-09-24 15:33:36 +0530

    feat(proxmox): manage control-plane VM with OpenTofu

--- infrastructure/proxmox/vms.tf
+++ infrastructure/proxmox/vms.tf
@@ -31,3 +31,41 @@ resource "proxmox_virtual_environment_vm" "dnsmasq" {
     firewall    = true
   }
 }
+
+resource "proxmox_virtual_environment_vm" "control_plane" {
+  name      = "control-plane"
+  node_name = "pve"
+  vm_id     = 101
+
+  on_boot = false
+
+  cpu {
+    cores = 4
+    type  = "x86-64-v2-AES"
+  }
+
+  bios = "seabios"
+
+  operating_system {
+    type = "l26"
+  }
+
+  memory {
+    dedicated = 5120
+  }
+
+  scsi_hardware = "virtio-scsi-single"
+
+  disk {
+    datastore_id = "local-lvm"
+    interface    = "scsi0"
+    size         = 32
+    iothread     = true
+  }
+
+  network_device {
+    bridge      = "vmbr0"
+    mac_address = "BC:24:11:13:D5:30"
+    firewall    = true
+  }
+}
